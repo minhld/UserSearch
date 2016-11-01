e85fa5c5bcb3aaf2f1b0924e2fd654945fb336cf
Le Dinh Minh <ledinhminh3883@gmail.com>
2016-10-31 23:46:13 -0700

    added the missing classes to project

---
+++ UserSearch/View/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace UserSearch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
